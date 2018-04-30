394a44e0fd911697e9295591dd395627637c6d1b
Ben Miller <bsmiller25@gmail.com>
2018-04-30 09:41:57 -0400

    simplified emacs init

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,34 +25,10 @@
       myPackages)
 
 (load-theme 'material t)
-(elpy-enable)
-(elpy-use-ipython)
-;;(setenv "IPY_TEST_SIMPLE_PROMPT" "1")
-;;(ansi-color-for-comint-mode-on)
-
-;; use flycheck not flymake with elpy
-;;(when (require 'flycheck nil t)
-;;  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-;;  (add-hook 'elpy-mode-hook 'flycheck-mode))
-
-;; enable autopep8 on save
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
 ;; ;; enable visual feedback on selections
 (setq transient-mark-mode t)
 
-;; ;; default to unified diffs
-(setq diff-switches "-u")
-
-;; ;; always end a file with a newline
-;;(setq require-final-newline 'query)
-
-;; Don't start emacs at the default screen
-(setq inhibit-startup-screen t)
-
-;; start emacs at the shell terminal emulator
-;;(switch-to-buffer (get-buffer-create (shell)))
 
 ;; make switch buffers give me list of available buffers
 (ido-mode 1)
@@ -68,9 +44,6 @@
 
 ;; keybindings
 
-;; terminal
-(global-set-key (kbd "<f1>") 'ansi-term)
-
 ;; multiple cursors
 ;; add cursor to all lines in selected region
 (global-set-key (kbd "C-c C-m") 'mc/edit-lines)
@@ -81,19 +54,18 @@
 (global-set-key (kbd "C-c C-a") 'mc/mark-all-like-this)
 
 
-;; Increase the default text size
-(set-face-attribute 'default nil :height 130)
+;;show line numbers everywhere
+;;global-linum-mode t)
 
-;; show line numbers everywhere
-;;(global-linum-mode t)
-
-;; cuspish colors
-;;(set-face-background 'default "white smoke")
-;;(set-face-foreground 'default "gray25")
-;;(set-face-foreground 'font-lock-comment-face "DarkOrange3")
-;;(set-face-foreground 'font-lock-string-face "RoyalBlue1")
-;;(set-face-foreground 'font-lock-function-name-face "black")
-;;(set-face-foreground 'font-lock-keyword-face "green4")
-;;(set-face-foreground 'font-lock-builtin-face "Red")
-
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
