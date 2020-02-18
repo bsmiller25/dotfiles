d5e826134ea2cd82c5b16b9bb00b3c42c59b6dc7
Ben Miller <bsmiller25@gmail.com>
2020-02-18 13:59:38 -0500

    add better spacing for webmode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -57,6 +57,12 @@
 	    ;; Default indentation is usually 2 spaces, changing to 4.
 ;;	    (set (make-local-variable 'sgml-basic-offset) 4)))
 
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
